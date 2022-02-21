ebe53baa1c95f4cc17518076d2ffbc48b4b0cbef
Turlough stelfox Mullan <duke1199sp@gmail.com>
2022-02-21 12:55:51 +0100

    Forgot to push this last tiem

--- venv/module00/ex04/operations.py
+++ venv/module00/ex04/operations.py
@@ -1,2 +1,21 @@
 import sys
 
+def	the_thing():
+	if len(sys.argv) > 3:
+		print("InputError: too many arguments\n")
+	if len(sys.argv) != 3:
+		print("Usage: python operations.py <num1> <num2>")
+		print("Example:\n\tpython operations/py 10 3")
+		return
+	num1 = int(sys.argv[1])
+	num2 = int(sys.argv[2])
+	# int(num1)
+	# int(num2)
+	print("Sum: ", num1 + num2)
+	print("Difference: ", num1 - num2)
+	print("Product: ", num1 * num2)
+	print("Quotient: ", num1 / num2)
+	print("Remainder: ", num1 % num2)
+
+if __name__ == '__main__':
+	the_thing()
