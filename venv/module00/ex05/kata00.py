8a43f7dea379ea3ea39f331d7f0eb9a9714473d0
Turlough stelfox Mullan <duke1199sp@gmail.com>
2022-02-23 18:26:42 +0100

    Not sure I'm doing this in the most python way?

---
+++ venv/module00/ex05/kata00.py
@@ -0,0 +1,5 @@
+t = (19, 42, 21)
+
+string = "The three numbers are: " + str(t[0]) + ", " + str(t[1]) + ", " + str(t[2])
+print(string)
+# print("The three numbers are: ", str(t[0]), ", ", str(t[1]), ", ", str(t[2]))
