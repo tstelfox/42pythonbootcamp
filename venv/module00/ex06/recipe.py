08176d7e75b2ea721ef678c0e08fe468d5dadc82
Turlough stelfox Mullan <duke1199sp@gmail.com>
2022-02-24 17:01:09 +0100

    Teodical day

---
+++ venv/module00/ex06/recipe.py
@@ -0,0 +1,8 @@
+cookbook = {
+    'sandwich': {'ingredients': ["ham", "bread", "cheese", "tomatoes"], 'meal': "lunch", 'prep_time': 10},
+    'cake': {'ingredients': ["flour", "sugar", "eggs"], 'meal': "dessert", 'prep_time': 60},
+    'salad': {'ingredients': ["avocado", "rucola", "toamtoes", "spinach" ], 'meal': "lunch", 'prep_time': 15}
+}
+
+for key in cookbook:
+    print(key)
