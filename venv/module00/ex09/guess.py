f5e81aabc474c2597ffecdb65e486aa1200eedad
Turlough Mullan <duke1199sp@gmail.com>
2022-02-28 18:41:22 +0100

    Bruh

--- venv/module00/ex09/guess.py
+++ venv/module00/ex09/guess.py
@@ -2,9 +2,14 @@ import random
 
 if __name__ == "__main__":
     answer = random.randint(1, 99)
+    # answer = 42
     i = 0
+    print("Guessin game.\nType exit to quit")
     while True:
         attempt = input("What's your guess between 1 and 99?\n>> ")
+        if attempt == 'exit':
+            print("Bye bitch")
+            exit()
         i += 1
         try:
             num = int(attempt)
@@ -16,7 +21,12 @@ if __name__ == "__main__":
         elif int(attempt) > answer:
             print("Too high!")
         else:
-            print("Congratulations, you've got it!\nYou won in", i, "attempts")
+            if answer == 42:
+                print("Stupid book reference")
+            if i == 1:
+                print("Got it on the first go!")
+            elif i > 1:
+                print("Congratulations, you've got it!\nYou won in", i, "attempts")
             exit()
 
 
