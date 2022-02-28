6a4a37dc588c23944d812a71749d7c82e5a67045
Turlough Mullan <duke1199sp@gmail.com>
2022-02-28 21:27:55 +0100

    Did I really learn about yield properly here?

---
+++ venv/module00/ex10/loading.py
@@ -0,0 +1,30 @@
+import time
+import os
+
+bar = 35
+
+def ft_progress(lst):
+    starttime = time.time()
+    for i in lst:
+        yield i
+        elapsed_time = time.time() - starttime
+        completed = int(i / (len(lst) / 100))
+        estimated = (elapsed_time / (i + 1)) * (len(lst) - i)
+        filled = int(bar * (completed / 100))
+        left = bar - filled
+        os.system("clear")
+        print(f"ETA: {estimated:.2f}s  [{completed}%][{filled * '='}>{left * ' '}] {i}/{len(lst)} | elapsed time {elapsed_time:.2f}s")
+    os.system("clear")
+    print(f"ETA: 0.0s  [100%][{(bar - 1) * '='}>] {i + 1}/{len(lst)} | elapsed time {elapsed_time:.2f}s")
+
+
+
+if __name__ == '__main__':
+    listy = range(200)
+    ret = 0
+    for elem in ft_progress(listy):
+        ret += elem
+        time.sleep(0.01)
+    print()
+    print("finished!")
+    print(ret)
